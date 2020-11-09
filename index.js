004afea9547fd50a7aa655836d5a4f45f9ee82b4
saifpirjade <saif.pirjade@gmail.com>
2020-11-09 09:58:47 +0530

    Created the server and handled server is running or not

--- index.js
+++ index.js
@@ -5,4 +5,17 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const momentTimezone = require("moment-timezone");
 const moment = require("moment");
+
+
+const app = express();
+const server = http.createServer(app);
 console.log("Running")
+
+const onListening = () => {
+    let addr = server.address();
+    let bind = typeof addr === 'string' ? 'pipe: ' + addr : 'port: ' + addr.port;
+    console.log(`Server Listening on ${bind} process id: ${process.pid}`);
+};
+server.listen(8001, "127.0.0.1")
+// server.on('error', onError);
+server.on('listening', onListening);
